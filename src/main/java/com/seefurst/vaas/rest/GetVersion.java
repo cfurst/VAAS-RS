65513df1023dcd91a72d41d96a8b369abf16a778
Carl Furst <carl.furst@seefurst.com>
2018-07-31 12:05:20 -0400

    get a specific version

--- src/main/java/com/seefurst/vaas/rest/GetVersion.java
+++ src/main/java/com/seefurst/vaas/rest/GetVersion.java
@@ -12,6 +12,7 @@ import javax.jcr.UnsupportedRepositoryOperationException;
 import javax.jcr.version.VersionManager;
 import javax.jcr.version.VersionHistory;
 import javax.jcr.version.Version;
+import javax.jcr.version.VersionException;
 import javax.jcr.Node;
 import javax.jcr.Workspace;
 
@@ -50,20 +51,52 @@ public class GetVersion implements VaasRestBase{
 			Session repoSess = (Session) req.getAttribute(REPOSITORY_SESSION_SERVLET_ATTRB_NAME);
 			Workspace wrkSp = repoSess.getWorkspace();
 			VersionManager vm = wrkSp.getVersionManager();
-			Version v = vm.getBaseVersion(contentName);
-			Node vn = v.getFrozenNode();
-			return Response.ok().encoding("utf-8").entity(vn.getProperty(NODE_CONTENT_PROPERTY_NAME).getString()).build();
-			 
+			Node root = repoSess.getRootNode();
+			if (root.hasNode(contentName)) {
+				Version v = vm.getBaseVersion(root.getNode(contentName).getPath());
+				Node vn = v.getFrozenNode();
+				return Response.ok().encoding("utf-8").entity(vn.getProperty(NODE_CONTENT_PROPERTY_NAME).getString()).build();
+				 
+			} else {
+				return Response.status(404, "{\"error\": \"The content " + contentName + " was not found or does not have any versions yet\"}").build();
+			}
+			
 		} catch (ClassCastException|UnsupportedRepositoryOperationException a) {
 			
 			return Response.serverError().encoding("utf-8").entity("{" +
 					"\"error:\"" + a.getLocalizedMessage() + "\"" +
 					"}").build();
 		} catch (RepositoryException b) {
-			return Response.noContent().build();
+			return Response.serverError().encoding("utf-8").entity("{\"error\": \"" + b.getLocalizedMessage() + "\"}").build();
 		}
 		
 		
+	}
+	
+	@Path("content/get/{contentName}/{versionName}")
+	@GET
+	@Produces("application/json")
+	public Response getVersion(@PathParam("contentName") String contentName, @PathParam("versionName") String versionName) {
+		try {
+			Session repoSess = (Session) req.getAttribute(REPOSITORY_SESSION_SERVLET_ATTRB_NAME);
+			Workspace wrkSp = repoSess.getWorkspace();
+			Node root = repoSess.getRootNode();
+			if (root.hasNode(contentName)) {
+				
+				VersionManager vm = wrkSp.getVersionManager();
+				VersionHistory vh = vm.getVersionHistory(root.getNode(contentName).getPath());
+				Version v = vh.getVersion(versionName);
+				Node f = v.getFrozenNode();
+				return Response.ok(f.getProperty(NODE_CONTENT_PROPERTY_NAME)).encoding("utf-8").build();
+			} else {
+				return Response.status(404, "{\"error\": \"The content " + contentName + " was not found\"}").build();
+			}
+			
+		} catch(VersionException a) {
+			return Response.status(404, "{\"error\": \"The content " + contentName + " does not have version " + versionName + "\"}").encoding("utf-8").build();
+		}catch (RepositoryException b) {
+			return Response.serverError().encoding("utf-8").entity("{\"error\": {\"" + b.getLocalizedMessage() + "\"}").build();
+		}
 	}
 	
 	
