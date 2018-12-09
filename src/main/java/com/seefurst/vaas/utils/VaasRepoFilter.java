30affc75aa929a52800d2a7a10c2d96886ba8349
Carl Furst <carl.furst@seefurst.com>
2018-12-08 19:24:13 -0500

    logging and null map.

--- src/main/java/com/seefurst/vaas/utils/VaasRepoFilter.java
+++ src/main/java/com/seefurst/vaas/utils/VaasRepoFilter.java
@@ -40,23 +40,27 @@ public class VaasRepoFilter implements Filter {
 			LOG.finest("got repourl: " + repositoryUrl);
 			Map<String, String> repoOptions = new HashMap<>();
 			//repoOptions.put("repository.home", repositoryUrl);
+			LOG.finest("getting repo with null map...");
 			Repository repo = (new OakRepositoryFactory()).getRepository(null);
-			
+			LOG.fine("Logging in...");
 			repoSess = repo.login(new SimpleCredentials(REPOSITORY_USERNAME,REPOSITORY_PASSWORD.toCharArray()));
-			
+			LOG.fine("done.. setting attribute...");
 			httpReq.setAttribute(REPOSITORY_SESSION_SERVLET_ATTRB_NAME, repoSess);
-			
+			LOG.finest("donig filter...");
 			chain.doFilter(req, res);
-			
+			LOG.finest("done filter... no errors...");
 		} catch (ClassCastException|ServletException|IOException|RepositoryException a){
 			
 			LOG.log(java.util.logging.Level.SEVERE, "Couldn't log into repository", a);
 			httpRes.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Couldn't login to responsitory");
 			
 		} finally {
-			if (repoSess != null)
-				repoSess.logout();
 			
+			LOG.fine("logging out..");
+			if (repoSess != null) {
+				LOG.finest("repoSess is not null and therefore calling logout()");
+				repoSess.logout();
+			}
 		}
 	}
 }
