67810ea2e79949b4eca122f161fd49e3d5e43b57
Carl Furst <carl.furst@seefurst.com>
2019-09-18 14:21:51 -0400

    remove @Consumes from @PUT handler

--- src/main/java/com/seefurst/vaas/rest/SaveVersion.java
+++ src/main/java/com/seefurst/vaas/rest/SaveVersion.java
@@ -5,7 +5,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
@@ -86,7 +85,6 @@ public class SaveVersion implements VaasRestBase{
 	
 	@PUT
 	@Produces("application/json")
-	@Consumes("application/json")
 	@Path("commit/{contentName}")
 	public Response commitVersionPut(@PathParam("contentName") String contentName) {
 		return commitVersion(contentName);
